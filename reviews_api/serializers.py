9f96a4bcb26e3a496cee53387effc148ebb7981b
Vickie Vien <vickie@DESKTOP-A7CACKI.localdomain>
2022-04-11 14:42:37 -0700

    removed test

--- reviews_api/serializers.py
+++ reviews_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Review
 class ReviewSerializer(serializers.ModelSerializer): 
     class Meta:
         model = Review
-        fields = ('id', 'reviewer_name', 'city', 'country', 'review', 'img', 'trip_start_date', 'trip_end_date', 'test',)
+        fields = ('id', 'reviewer_name', 'city', 'country', 'review', 'img', 'trip_start_date', 'trip_end_date',)
